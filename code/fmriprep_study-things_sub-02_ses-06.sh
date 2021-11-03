0dec5572c1148e645b4b50d5eb1f86f4973950e5
bpinsard <basile.pinsard@gmail.com>
2021-11-03 16:49:20 -0400

    [DATALAD] Recorded changes

--- code/fmriprep_study-things_sub-02_ses-06.sh
+++ code/fmriprep_study-things_sub-02_ses-06.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=fmriprep_study-things_sub-02_ses-06.job
 #SBATCH --output=/lustre03/project/6003287/datasets/cneuromod_processed/fmriprep/things/code/fmriprep_study-things_sub-02_ses-06.out
 #SBATCH --error=/lustre03/project/6003287/datasets/cneuromod_processed/fmriprep/things/code/fmriprep_study-things_sub-02_ses-06.err
-#SBATCH --time=12:00:00
-#SBATCH --cpus-per-task=16
+#SBATCH --time=8:00:00
+#SBATCH --cpus-per-task=12
 #SBATCH --mem-per-cpu=4096M
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
@@ -17,9 +17,9 @@ export SINGULARITYENV_TEMPLATEFLOW_HOME="sourcedata/templateflow/"
 
 
 export LOCAL_DATASET=$SLURM_TMPDIR/${SLURM_JOB_NAME//-/}/
-flock --verbose /lustre03/project/6003287/datasets/cneuromod_processed/fmriprep/things/.datalad_lock datalad clone /lustre03/project/6003287/datasets/cneuromod_processed/fmriprep/things $LOCAL_DATASET
+flock --verbose /project/rrg-pbellec/ria-beluga/alias/things.fmriprep/.datalad_lock datalad clone ria+file:///project/rrg-pbellec/ria-beluga#~things.fmriprep $LOCAL_DATASET
 cd $LOCAL_DATASET
-datalad get -n -r -R1 . # get sourcedata/*
+datalad get -s ria-beluga-storage -n -r -R1 . # get sourcedata/* containers
 datalad get -s ria-beluga-storage -r sourcedata/templateflow/tpl-{MNI152NLin2009cAsym,OASIS30ANTs,fsLR,fsaverage,MNI152NLin6Asym}
 if [ -d sourcedata/smriprep ] ; then
     datalad get -n sourcedata/smriprep sourcedata/smriprep/sourcedata/freesurfer
@@ -31,13 +31,13 @@ if [ -d sourcedata/freesurfer ] ; then
 fi
 
 
-datalad containers-run -m 'fMRIPrep_sub-02/ses-06' -n containers/bids-fmriprep --input sourcedata/things/sub-02/ses-06/fmap/ --input sourcedata/things/sub-02/ses-06/func/ --input sourcedata/templateflow/tpl-MNI152NLin2009cAsym/ --input sourcedata/templateflow/tpl-OASIS30ANTs/ --input sourcedata/templateflow/tpl-fsLR/ --input sourcedata/templateflow/tpl-fsaverage/ --input sourcedata/templateflow/tpl-MNI152NLin6Asym/ --output . --input 'sourcedata/smriprep/sub-02/anat/' --input sourcedata/smriprep/sourcedata/freesurfer/fsaverage/ --input sourcedata/smriprep/sourcedata/freesurfer/sub-02/ -- -w ./workdir --participant-label 02 --anat-derivatives ./sourcedata/smriprep --fs-subjects-dir ./sourcedata/smriprep/sourcedata/freesurfer --bids-filter-file code/fmriprep_study-things_sub-02_ses-06_bids_filters.json --output-layout bids --ignore slicetiming --use-syn-sdc --output-spaces MNI152NLin2009cAsym T1w:res-iso2mm --cifti-output 91k --notrack --write-graph --skip_bids_validation --omp-nthreads 8 --nprocs 16 --mem_mb 65536 --fs-license-file code/freesurfer.license --resource-monitor sourcedata/things ./ participant 
+datalad containers-run -m 'fMRIPrep_sub-02/ses-06' -n containers/bids-fmriprep --input sourcedata/things/sub-02/ses-06/fmap/ --input sourcedata/things/sub-02/ses-06/func/ --input sourcedata/templateflow/tpl-MNI152NLin2009cAsym/ --input sourcedata/templateflow/tpl-OASIS30ANTs/ --input sourcedata/templateflow/tpl-fsLR/ --input sourcedata/templateflow/tpl-fsaverage/ --input sourcedata/templateflow/tpl-MNI152NLin6Asym/ --output . --input 'sourcedata/smriprep/sub-02/anat/' --input sourcedata/smriprep/sourcedata/freesurfer/fsaverage/ --input sourcedata/smriprep/sourcedata/freesurfer/sub-02/ -- -w ./workdir --participant-label 02 --anat-derivatives ./sourcedata/smriprep --fs-subjects-dir ./sourcedata/smriprep/sourcedata/freesurfer --bids-filter-file code/fmriprep_study-things_sub-02_ses-06_bids_filters.json --output-layout bids  --use-syn-sdc --output-spaces MNI152NLin2009cAsym T1w:res-iso2mm --cifti-output 91k --notrack --write-graph --skip_bids_validation --omp-nthreads 8 --nprocs 12 --mem_mb 49152 --fs-license-file code/freesurfer.license --resource-monitor sourcedata/things ./ participant 
 fmriprep_exitcode=$?
 
-flock --verbose /lustre03/project/6003287/datasets/cneuromod_processed/fmriprep/things/.datalad_lock datalad push -d ./ --to origin
+flock --verbose /project/rrg-pbellec/ria-beluga/alias/things.fmriprep/.datalad_lock datalad push -d ./ --to origin
 if [ -d sourcedata/freesurfer ] ; then
-    flock --verbose /lustre03/project/6003287/datasets/cneuromod_processed/fmriprep/things/.datalad_lock datalad push -d sourcedata/freesurfer $LOCAL_DATASET --to origin
+    flock --verbose /project/rrg-pbellec/ria-beluga/alias/things.fmriprep/.datalad_lock datalad push -d sourcedata/freesurfer $LOCAL_DATASET --to origin
 fi 
-if [ -e $LOCAL_DATASET/resource_monitor.json ] ; cp $LOCAL_DATASET/resource_monitor.json /scratch/bpinsard/fmriprep_study-things_sub-02_ses-06_resource_monitor.json ; fi 
+if [ -e $LOCAL_DATASET/workdir/fmriprep_wf/resource_monitor.json ] ; then cp $LOCAL_DATASET/workdir/fmriprep_wf/resource_monitor.json /scratch/bpinsard/fmriprep_study-things_sub-02_ses-06_resource_monitor.json ; fi 
 if [ $fmriprep_exitcode -ne 0 ] ; then cp -R $LOCAL_DATASET /scratch/bpinsard/fmriprep_study-things_sub-02_ses-06 ; fi 
 exit $fmriprep_exitcode 
