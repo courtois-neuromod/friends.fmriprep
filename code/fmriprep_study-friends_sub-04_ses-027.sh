325d2a8c554611fcc9756f9692a3f9390686b73a
bpinsard <basile.pinsard@gmail.com>
2022-05-13 16:22:02 -0400

    [DATALAD] Recorded changes

--- code/fmriprep_study-friends_sub-04_ses-027.sh
+++ code/fmriprep_study-friends_sub-04_ses-027.sh
@@ -21,8 +21,8 @@ export SINGULARITYENV_TEMPLATEFLOW_HOME="sourcedata/templateflow/"
 export LOCAL_DATASET=$SLURM_TMPDIR/${SLURM_JOB_NAME//-/}/
 flock --verbose /lustre03/project/rrg-pbellec/ria-beluga/alias/cneuromod.friends.fmriprep/.datalad_lock datalad clone ria+file:///lustre03/project/rrg-pbellec/ria-beluga#~cneuromod.friends.fmriprep $LOCAL_DATASET
 cd $LOCAL_DATASET
-datalad get -s ria-beluga-storage -n -r -R1 . # get sourcedata/* containers
-datalad get -s ria-beluga-storage -r sourcedata/templateflow/tpl-{MNI152NLin2009cAsym,OASIS30ANTs,fsLR,fsaverage,MNI152NLin6Asym}
+datalad get -s ria-beluga-storage -J 4 -n -r -R1 . # get sourcedata/* containers
+datalad get -s ria-beluga-storage -J 4 -r sourcedata/templateflow/tpl-{MNI152NLin2009cAsym,OASIS30ANTs,fsLR,fsaverage,MNI152NLin6Asym}
 if [ -d sourcedata/smriprep ] ; then
     datalad get -n sourcedata/smriprep sourcedata/smriprep/sourcedata/freesurfer
 fi
@@ -33,12 +33,12 @@ if [ -d sourcedata/freesurfer ] ; then
 fi
 
 
-datalad containers-run -m 'fMRIPrep_sub-04/ses-027' -n containers/bids-fmriprep --input sourcedata/friends/sub-04/ses-027/fmap/ --input sourcedata/friends/sub-04/ses-027/func/ --input sourcedata/templateflow/tpl-MNI152NLin2009cAsym/ --input sourcedata/templateflow/tpl-OASIS30ANTs/ --input sourcedata/templateflow/tpl-fsLR/ --input sourcedata/templateflow/tpl-fsaverage/ --input sourcedata/templateflow/tpl-MNI152NLin6Asym/ --output . --input 'sourcedata/smriprep/sub-04/anat/' --input sourcedata/smriprep/sourcedata/freesurfer/fsaverage/ --input sourcedata/smriprep/sourcedata/freesurfer/sub-04/ -- -w ./workdir --participant-label 04 --anat-derivatives ./sourcedata/smriprep --fs-subjects-dir ./sourcedata/smriprep/sourcedata/freesurfer --bids-filter-file code/fmriprep_study-friends_sub-04_ses-027_bids_filters.json --output-layout bids --ignore slicetiming --use-syn-sdc --output-spaces MNI152NLin2009cAsym T1w:res-iso2mm --cifti-output 91k --notrack --write-graph --skip_bids_validation --omp-nthreads 8 --nprocs 12 --mem_mb 49152 --fs-license-file code/freesurfer.license --resource-monitor sourcedata/friends ./ participant 
+datalad containers-run -m 'fMRIPrep_sub-04/ses-027' -n containers/bids-fmriprep --input sourcedata/friends/sub-04/ses-027/fmap/ --input sourcedata/friends/sub-04/ses-027/func/ --input sourcedata/templateflow/tpl-MNI152NLin2009cAsym/ --input sourcedata/templateflow/tpl-OASIS30ANTs/ --input sourcedata/templateflow/tpl-fsLR/ --input sourcedata/templateflow/tpl-fsaverage/ --input sourcedata/templateflow/tpl-MNI152NLin6Asym/ --output . --input 'sourcedata/smriprep/sub-04/anat/' --input sourcedata/smriprep/sourcedata/freesurfer/fsaverage/ --input sourcedata/smriprep/sourcedata/freesurfer/sub-04/ -- -w ./workdir --participant-label 04 --anat-derivatives ./sourcedata/smriprep --fs-subjects-dir ./sourcedata/smriprep/sourcedata/freesurfer --bids-filter-file code/fmriprep_study-friends_sub-04_ses-027_bids_filters.json --output-layout bids  --use-syn-sdc --output-spaces MNI152NLin2009cAsym T1w:res-iso2mm --cifti-output 91k --notrack --write-graph --skip_bids_validation --omp-nthreads 8 --nprocs 12 --mem_mb 49152 --fs-license-file code/freesurfer.license --resource-monitor sourcedata/friends ./ participant 
 fmriprep_exitcode=$?
 
 flock --verbose /lustre03/project/rrg-pbellec/ria-beluga/alias/cneuromod.friends.fmriprep/.datalad_lock datalad push -d ./ --to origin
 if [ -d sourcedata/freesurfer ] ; then
-    flock --verbose /lustre03/project/rrg-pbellec/ria-beluga/alias/cneuromod.friends.fmriprep/.datalad_lock datalad push -d sourcedata/freesurfer $LOCAL_DATASET --to origin
+    flock --verbose /lustre03/project/rrg-pbellec/ria-beluga/alias/cneuromod.friends.fmriprep/.datalad_lock datalad push -J 4 -d sourcedata/freesurfer $LOCAL_DATASET --to origin
 fi 
 if [ -e $LOCAL_DATASET/workdir/fmriprep_wf/resource_monitor.json ] ; then cp $LOCAL_DATASET/workdir/fmriprep_wf/resource_monitor.json /scratch/bpinsard/fmriprep_study-friends_sub-04_ses-027_resource_monitor.json ; fi 
 if [ $fmriprep_exitcode -ne 0 ] ; then cp -R $LOCAL_DATASET /scratch/bpinsard/fmriprep_study-friends_sub-04_ses-027 ; fi 
